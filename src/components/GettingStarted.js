0f874508958d9c461d54785cb12052f7b5a23d01
MaxBlaushild <blaushmild@gmail.com>
2018-08-07 10:35:07 -0400

    adds links to getting started section

--- src/components/GettingStarted.js
+++ src/components/GettingStarted.js
@@ -46,14 +46,16 @@ class GettingStarted extends Component {
             <Grid item xs={12} sm={6} md={4} className={classes.item}>
               <div className={classes.copy}>
                 <Typography variant='title'>
-                  LinniaJS
+                  Linnia JS
                 </Typography>
                 <Typography variant='body1'>
                   LinniaJS is our Javascript SDK. This library allows you to start building on top of our smart contracts immediately.
                 </Typography>
-                <Button className={classes.ctaButton}>
-                  Check it Out
-                </Button>
+                <a href="https://github.com/ConsenSys/linnia-js">
+                  <Button className={classes.ctaButton}>
+                    Check it Out
+                  </Button>
+                </a>
               </div>
             </Grid>
             <Grid item xs={12} sm={6} md={4} className={classes.item}>
@@ -64,9 +66,11 @@ class GettingStarted extends Component {
                 <Typography variant='body1'>
                   The Linnia Box is a React UI template that comes with baked in Linnia Protocol and IPFS integration.
                 </Typography>
-                <Button className={classes.ctaButton}>
-                  Try it out
-                </Button>
+                <a href="https://github.com/ConsenSys/linnia-box">
+                  <Button className={classes.ctaButton}>
+                    Try it out
+                  </Button>
+                </a>
               </div>
             </Grid>
             <Grid item xs={12} sm={6} md={4} className={classes.item}>
@@ -77,9 +81,11 @@ class GettingStarted extends Component {
                 <Typography variant='body1'>
                   Linnia Node is a NodeJS server that captures all of the records and permissions stored in the Linnia and allows you to query them.
                 </Typography>
-                <Button className={classes.ctaButton}>
-                  Check it Out
-                </Button>
+                <a href="https://github.com/ConsenSys/linnia-server">
+                  <Button className={classes.ctaButton}>
+                    Check it Out
+                  </Button>
+                </a>
               </div>
             </Grid>
           </Grid>
